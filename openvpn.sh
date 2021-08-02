a796c0a28e258b88c4dbda865fc83504876f38d2
Ronald van Kouwen <Ronaldvk90@outlook.com>
2021-08-02 17:34:46 +0200

    Update openvpn.sh
    Updated to Alpine Edge

--- openvpn.sh
+++ openvpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker volume create ovpnkeys
-docker build . -t vpnserver/1.0
-docker run --restart=always --name openvpn -d -v ovpnkeys:/etc/openvpn/keys -p 1194:1194/tcp --cap-add=NET_ADMIN -t vpnserver/1.0
+docker build . -t vpnserver/2.0
+docker run --restart=always --name openvpn -d -v ovpnkeys:/etc/openvpn/keys -p 1194:1194/tcp --cap-add=NET_ADMIN -t vpnserver/2.0
