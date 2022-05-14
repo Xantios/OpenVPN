8e72527fc21aaf99b6a3f9fe95258231df189a52
Xantios Krugor <git@xantios.nl>
2022-05-14 14:09:12 +0000

    Remove old container before starting new one and add explicit deive mapping for nested container

--- openvpn.sh
+++ openvpn.sh
@@ -2,4 +2,5 @@
 
 docker volume create ovpnkeys
 docker build . -t vpnserver/2.0
-docker run --restart=always --name openvpn -d -v ovpnkeys:/etc/openvpn/keys -p 1194:1194/tcp --cap-add=NET_ADMIN -t vpnserver/2.0
+docker rm -f openvpn
+docker run --restart=always --name openvpn -v ovpnkeys:/etc/openvpn/keys -p 1194:1194/tcp --cap-add=NET_ADMIN --device=/dev/net/tun -t vpnserver/2.0
